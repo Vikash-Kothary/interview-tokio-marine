da4bc18dab62cae2845032574feb9bfe92b876df
Vikash Kothary <kothary.vikash@gmail.com>
2025-01-28 09:43:36 +0000

    test: Add placeholder integration tests

---
+++ tests/tokio_marine/integration/repositories/test_policy_repository.py
@@ -0,0 +1,11 @@
+
+# NOTE: Test from repostory context to infrastructure.
+
+def test_in_memory_policy_repository():
+    pass
+
+def test_json_policy_repository():
+    pass
+
+def test_postgres_policy_repository():
+    pass
