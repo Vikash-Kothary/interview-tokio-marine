7eb268c29d93d0e99b9b6deac102c92247bbb952
Vikash Kothary <kothary.vikash@gmail.com>
2025-01-28 09:45:12 +0000

    fix: Add self argument to not found exception

--- src/tokio_marine/domain/exceptions.py
+++ src/tokio_marine/domain/exceptions.py
@@ -2,8 +2,10 @@
 class DomainError(Exception):
     pass
 
+# NOTE: Domain specific error cases
+
 class PolicyNotFoundException(NotImplementedError):
-    def __init__(policy_id: str):
+    def __init__(self, policy_id: str):
         super().__init__(f'{policy_id=} not found')
 
 class NotImplementedException(NotImplementedError):
